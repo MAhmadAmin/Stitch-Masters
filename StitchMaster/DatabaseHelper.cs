cca55f27ff5fad3c58dc43456e040816feab8933
M Ahmad Amin <ahmadamin1005@gmail.com>
2025-05-03 00:25:08 +0500

    Add DatabaseHelper Class 1st Version

---
+++ StitchMaster/DatabaseHelper.cs
@@ -0,0 +1,60 @@
+﻿using MySql.Data.MySqlClient;
+
+namespace StitchMaster
+{
+    public class DatabaseHelper
+    {
+        private readonly string connectionString;
+
+        private DatabaseHelper()
+        {
+            var config = LoadConfiguration();
+            connectionString = config.GetConnectionString("MyDbConnection");
+        }
+        private static IConfiguration LoadConfiguration()
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(AppContext.BaseDirectory) // For console/WinForms apps
+                .AddJsonFile("appsettings.development.json", optional: false, reloadOnChange: true);
+            return builder.Build();
+        }
+        private static DatabaseHelper _instance;
+        public static DatabaseHelper Instance
+        {
+            get
+            {
+                if (_instance == null) _instance = new DatabaseHelper();
+                return _instance;
+            }
+        }
+        public MySqlConnection getConnection()
+        {
+            var connection = new MySqlConnection(connectionString);
+            connection.Open();
+
+            return connection;
+        }
+        public MySqlDataReader getData(string query)
+        {
+            using (var connection = getConnection())
+            {
+                using (var command = new MySqlCommand(query, getConnection()))
+                {
+                    return command.ExecuteReader();
+                }
+            }
+
+        }
+        public int ExecuteQuery(string query)
+        {
+            using (var connection = getConnection())
+            {
+                using (var command = new MySqlCommand(query, getConnection()))
+                {
+                    return command.ExecuteNonQuery();
+                }
+            }
+
+        }
+    }
+}
