53c38457b758c6d83189279134257fb0e3062655
M Ahmad Amin <ahmadamin1005@gmail.com>
2025-05-08 01:44:40 +0500

    Added SIgn up as

---
+++ StitchMaster/Components/Pages/Signup-as.razor.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace StitchMaster.Components.Pages
+{
+    public partial class Signup_as
+    {
+        [Inject]
+        private NavigationManager NavigationManager { get; set; }
+        private void NavigateTo(UserType userType)
+        {
+            if (userType == UserType.Customer)
+                NavigationManager.NavigateTo("/signup/signup-as-customer");
+            else if(userType == UserType.Tailor)
+                NavigationManager.NavigateTo("/signup/signup-as-tailor");
+            else if (userType == UserType.FabricStore)
+                NavigationManager.NavigateTo("/signup/signup-as-fabricstore");
+        }
+
+        private enum UserType
+        {
+            Customer,
+            Tailor,
+            FabricStore
+        }
+    }
+}
