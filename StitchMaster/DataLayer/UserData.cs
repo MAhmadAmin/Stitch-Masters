f5ab7e61bb1ab48a494d2070d2fcc056aa98e29a
M Ahmad Amin <ahmadamin1005@gmail.com>
2025-05-07 22:57:22 +0500

    UserDL Changes

--- StitchMaster/DataLayer/UserData.cs
+++ StitchMaster/DataLayer/UserData.cs
@@ -7,12 +7,12 @@ namespace StitchMaster.DataLayer
     {
         static public int StoreUser(User user)
         {
-            string query = $"INSERT INTO Users (username, email, password_hash, role_id) Values ('{user.Username}', '{user.Email}', '{user.Password}', '{user.UserRole.RoleID}')";
+            string query = $"INSERT INTO Users (username, email, hashed_password, role_id) Values ('{user.Username}', '{user.Email}', '{user.Password}', '{user.UserRole.RoleID}')";
             return DatabaseHelper.Instance.ExecuteQuery(query);
         }
         static public bool IsValidUser(string email, string password)
         {
-            DataTable dt = DatabaseHelper.Instance.GetDataTable($"SELECT * FROM users WHERE email='{email}' AND password_hash='{password}'");
+            DataTable dt = DatabaseHelper.Instance.GetDataTable($"SELECT * FROM users WHERE email='{email}' AND hashed_password='{password}'");
             if (dt.Rows.Count == 1)
                 return true;
             else
