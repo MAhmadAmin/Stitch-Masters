92ea3119bb05f548303c9d8790ca9e292233d50d
Muhammad Qasim Hamid <mqh.qasimhamid@gmail.com>
2025-05-10 05:02:26 -0700

    Imgae File Upload

--- StitchMaster/DataLayer/FabricStoreData.cs
+++ StitchMaster/DataLayer/FabricStoreData.cs
@@ -1,4 +1,9 @@
-﻿namespace StitchMaster.DataLayer
+﻿using StitchMaster.BusinessLogic;
+using StitchMaster.HelperClasses;
+using StitchMaster;
+using System.Data;
+
+namespace StitchMaster
 {
     public class FabricStoreData
     {
@@ -22,6 +27,18 @@
                 }
                 return _fabricStoreData;
             }
+        }
+        public List<FabricStore> GetAllObjects()
+        {
+            List<FabricStore> allStores = new List<FabricStore>();
+            string sql = $"Select * from fabric_store as f natural join users as u inner join lookup as l on u.role_id = l.lookup_id";
+            DataTable dt = DatabaseHelper.Instance.GetDataTable(sql);
+            foreach(DataRow dr in dt.Rows)
+            {
+                FabricStore store = new FabricStore(int.Parse(dr["f.store_id"].ToString()), dr["f.description"].ToString(),null,int.Parse( dr["f.user_id"].ToString()), dr["u.username"].ToString(), dr["u.name"].ToString(), dr["u.email"].ToString(), dr["u.hashed_password"].ToString(), dr["u.profile_img_url"].ToString(),DateTime.Parse(dr["u.created_at"].ToString()), new UserRole(int.Parse(dr["l.lookup_id"].ToString()), dr["l.value"].ToString())  );
+                allStores.Add(store);
+            }
+            return allStores;
         }
     }
 }
