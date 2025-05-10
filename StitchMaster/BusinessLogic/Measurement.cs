ed5f409efed27e1d9475fb3adaed8298d0fc24e0
UmarRana2005 <umerrana2k05@gmail.com>
2025-05-10 14:26:38 +0500

    Customer measurements

---
+++ StitchMaster/BusinessLogic/Measurement.cs
@@ -0,0 +1,20 @@
+﻿namespace StitchMaster.BusinessLogic
+{
+    public class Measurement
+    {
+        public int MeasurementId { get; set; }
+        public string MeasurementName { get; set; }
+        public int BuyerId { get; set; }
+        public string Gender { get; set; }
+
+        public decimal? Chest { get; set; }
+        public decimal? Waist { get; set; }
+        public decimal? ShoulderWidth { get; set; }
+        public decimal? SleveLength { get; set; }
+        public decimal? NeckSize { get; set; }
+        public decimal? Inseam { get; set; }
+        public decimal? Outseam { get; set; }
+        public decimal? Thigh { get; set; }
+        public decimal? Knee { get; set; }
+    }
+}
