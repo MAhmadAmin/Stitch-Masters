8dca742c2523ccd6f037e40a34cfff716efc3894
UmarRana2005 <umerrana2k05@gmail.com>
2025-05-11 11:37:17 +0500

    gig setup

--- StitchMaster/DataLayer/TailorGigData.cs
+++ StitchMaster/DataLayer/TailorGigData.cs
@@ -1,4 +1,8 @@
-﻿namespace StitchMaster.DataLayer
+﻿using MySql.Data.MySqlClient;
+using StitchMaster.BusinessLogic;
+using StitchMaster.HelperClasses;
+
+namespace StitchMaster.DataLayer
 {
     public class TailorGigData
     {
@@ -23,5 +27,90 @@
                 return _tailorGigData;
             }
         }
+
+        public List<TailorGig> GetAllGigs()
+        {
+            List<TailorGig> gigs = new List<TailorGig>();
+
+            string query = @"SELECT g.gig_id, g.title, g.description, g.price, g.delivery_time, g.image_url,
+                            c.category_id, c.category_name,c.gender
+                     FROM gig g
+                     INNER JOIN category c ON g.category_id = c.category_id";
+
+            MySqlDataReader reader = DatabaseHelper.Instance.getDataReader(query);
+
+            while (reader.Read())
+            {
+                Category category = new Category(
+                    Convert.ToInt32(reader["category_id"]),
+                    reader["category_name"].ToString(),
+                    reader["gender"].ToString()
+                );
+
+                TailorGig gig = new TailorGig(
+                    Convert.ToInt32(reader["gig_id"]),
+                    reader["title"].ToString(),
+                    reader["description"].ToString(),
+                    category,
+                    Convert.ToInt32(reader["price"]),
+                    Convert.ToInt32(reader["delivery_time"]),
+                    reader["image_url"].ToString()
+                );
+
+                gigs.Add(gig);
+            }
+
+            reader.Close();
+            return gigs;
+        }
+        public TailorGig? GetGigById(int gigId)
+        {
+            string query = $@"SELECT g.gig_id, g.title, g.description, g.price, g.delivery_time, g.image_url,
+                             c.category_id, c.category_name
+                      FROM gig g
+                      INNER JOIN category c ON g.category_id = c.category_id
+                      WHERE g.gig_id = {gigId}";
+
+            MySqlDataReader reader = DatabaseHelper.Instance.getDataReader(query);
+
+            if (reader.Read())
+            {
+                Category category = new Category(
+                    Convert.ToInt32(reader["category_id"]),
+                    reader["category_name"].ToString(),
+                    reader["gender"].ToString()
+                );
+
+                TailorGig gig = new TailorGig(
+                    Convert.ToInt32(reader["gig_id"]),
+                    reader["title"].ToString(),
+                    reader["description"].ToString(),
+                    category,
+                    Convert.ToInt32(reader["price"]),
+                    Convert.ToInt32(reader["delivery_time"]),
+                    reader["image_url"].ToString()
+                );
+
+                reader.Close();
+                return gig;
+            }
+
+            reader.Close();
+            return null;
+        }
+
+        public int GetGigOwner(int gigId)
+        {
+            string query = $"SELECT tailor_id FROM gig WHERE gig_id = {gigId}";
+
+            MySqlDataReader reader = DatabaseHelper.Instance.getDataReader(query);
+            if (reader.Read())
+            {
+                return Convert.ToInt32(reader["tailor_id"]);
+            }
+
+                throw new InvalidOperationException("Gig not found or tailor_id is invalid.");
+        }
+
     }
 }
